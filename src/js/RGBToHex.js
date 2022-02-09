8e03fe2973c865d67d8635f988a9cb511f82d630
David Paredes <dparedes@carpages.ca>
2022-02-09 18:57:40 -0500

    change RGBformElements map to be wrapped by React.Children.toArray instead of explicitly assigning key

--- src/js/RGBToHex.js
+++ src/js/RGBToHex.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import TitleButton from '../js/TitleButton';
 import Converted from '../js/Converted';
 
@@ -11,17 +11,14 @@ const RGBToHex = props => {
 
   let RGBformElements = [{
     label: 'Red',
-    key: 'red',
     value: red,
     onChange: setRed
   }, {
     label: 'Green',
-    key: 'green',
     value: green,
     onChange: setGreen
   }, {
     label: 'Blue',
-    key: 'blue',
     value: blue,
     onChange: setBlue
   }]
@@ -32,18 +29,20 @@ const RGBToHex = props => {
   }
 
   const rgbToHex = (r, g, b) => {
-    return "#" + componentToHex(parseInt(r)) 
-               + componentToHex(parseInt(g)) 
+    return "#" + componentToHex(parseInt(r))
+               + componentToHex(parseInt(g))
                + componentToHex(parseInt(b));
   }
 
   // RGB to HEX form submit
   const onSubmitRGB = (e) => {
     e.preventDefault();
-    
+
     let converted = rgbToHex(red, green, blue);
     setConvertedValue(converted);
   }
+
+  // {React.Children.toArray(myList.map(item => <div>Hello</div>))}
 
   return (
     <div className="uk-container uk-flex uk-flex-wrap uk-flex-center uk-position-center">
@@ -52,25 +51,25 @@ const RGBToHex = props => {
         <form onSubmit={onSubmitRGB}>
           <fieldset className=" uk-width-2xlarge@m uk-fieldset uk-margin-auto">
             <div className="uk-column-1-1 uk-column-1-3@s">
-              {RGBformElements.map(formElement => {
+              {React.Children.toArray(RGBformElements.map(formElement => {
                 return (
-                  <div key={formElement.key}>
-                    <label className="uk-form-label" htmlFor={formElement.key}>{formElement.label}</label>
-                    <input id={formElement.key} 
-                           name={formElement.key} 
-                           className="uk-input uk-margin-small-bottom" 
-                           type="number" 
-                           min="0" 
-                           max="255" 
-                           maxLength="3" 
-                           placeholder="255" 
+                  <div>
+                    <label className="uk-form-label" htmlFor={formElement.label}>{formElement.label}</label>
+                    <input id={formElement.label}
+                           name={formElement.label}
+                           className="uk-input uk-margin-small-bottom"
+                           type="number"
+                           min="0"
+                           max="255"
+                           maxLength="3"
+                           placeholder="255"
                            require="true"
                            value={formElement.value}
                            onChange={(e)=> {formElement.onChange(e.target.value)}}
                     />
                   </div>
                 )
-              })}
+              }))}
             </div>
           </fieldset>
           <input className="uk-button uk-align-center" name="submit" type="submit" value="Submit" />
